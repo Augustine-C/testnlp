1797a2f03ec376a9ad9fe3e85f34aedd7333962b
Yifan Cui <zjgdxbs@me.com>
2020-09-09 23:56:07 -0400

    initial setup

---
+++ src/main/java/nlptest/Nlptest.java
@@ -0,0 +1,70 @@
+package nlptest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.PartOfSpeechAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.semgraph.SemanticGraph;
+import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
+import edu.stanford.nlp.trees.Tree;
+import edu.stanford.nlp.trees.TreeCoreAnnotations;
+import edu.stanford.nlp.util.CoreMap;
+
+
+public class Nlptest
+{
+    public static void main( String[] args )
+    {
+        // creates a StanfordCoreNLP object, with POS tagging, lemmatization,
+        // NER, parsing, and coreference resolution
+        Properties props = new Properties();
+        props.setProperty("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+        // read some text in the text variable
+        String text = "Pick up the red block to the left of that blue block.";
+        // create an empty Annotation just with the given text
+        Annotation document = new Annotation(text);
+
+        // run all Annotators on this text
+        pipeline.annotate(document);
+
+        // these are all the sentences in this document
+        List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+
+        List<String> words = new ArrayList<>();
+        List<String> posTags = new ArrayList<>();
+        List<String> nerTags = new ArrayList<>();
+        for (CoreMap sentence : sentences) {
+            // traversing the words in the current sentence
+            Tree tree = sentence.get(TreeCoreAnnotations.TreeAnnotation.class);
+            System.out.println(tree);
+            SemanticGraph dependencies = sentence.get(SemanticGraphCoreAnnotations.BasicDependenciesAnnotation.class);
+            System.out.println(dependencies);
+            for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
+                // this is the text of the token
+                String word = token.get(TextAnnotation.class);
+                words.add(word);
+                // this is the POS tag of the token
+                String pos = token.get(PartOfSpeechAnnotation.class);
+                posTags.add(pos);
+                // this is the NER label of the token
+                String ne = token.get(NamedEntityTagAnnotation.class);
+                nerTags.add(ne);
+            }
+        }
+        System.out.println(words.toString());
+        System.out.println(posTags.toString());
+        System.out.println(nerTags.toString());
+
+        System.out.println( "End of Processing" );
+    }
+}
